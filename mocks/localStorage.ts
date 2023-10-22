9d82804d9aee089abdf4b69142a78a0dee011e1b
Centaurioun <86323645+Centaurioun@users.noreply.github.com>
2023-10-23 00:13:44 +0300

    Delete mocks/localStorage.ts

--- mocks/localStorage.ts
+++
@@ -1,9 +0,0 @@
-Object.defineProperty(window, "localStorage", {
-    value: {
-        getItem: jest.fn(() => null),
-        setItem: jest.fn(() => null)
-    },
-    writable: true
-});
-
-export const localStorageSetMock = window.localStorage.setItem;
